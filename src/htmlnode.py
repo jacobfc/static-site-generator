8c8bec030fb5c20ad4d97a6c7749006889162b9c
Jacob Christiansen <22478132+jacobfc@users.noreply.github.com>
2025-04-02 20:29:39 +0200

    Refactored Leafnode class into same python file as HTMLNode (parent class), along with unit tests.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,3 +25,20 @@ class HTMLNode:
             f"HTMLNode(tag={self.tag}, value={self.value},"
             f" children={self.children}, props={self.props})"
         )
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        assert value is not None  # LeafNode must have value specified
+        super().__init__(tag=tag, value=value, props=props)
+        assert self.children is None  # LeafNode explicitly has no children
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All LeafNodes must have a value")
+
+        if self.tag is None:
+            return self.value
+
+        html_full = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return html_full
