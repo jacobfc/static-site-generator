d6ba30a01bfe23ef897806a614e218dd7ecf087f
Jacob Christiansen <22478132+jacobfc@users.noreply.github.com>
2025-04-21 13:14:16 +0200

    Added block_to_block_type function, to identify types of markdown blocks

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "HEADING"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
 
@@ -8,3 +20,91 @@ def markdown_to_blocks(markdown):
             non_empty_blocks.append(stripped_block)
 
     return non_empty_blocks
+
+
+def block_to_block_type(markdown_block):
+    first_character = markdown_block[0]
+    # choose which checks to perform, based on first character in block
+    match first_character:
+        case "#":  # check if block is a  heading
+            if block_is_heading(markdown_block):
+                return BlockType.HEADING
+        case "`":  # check if block is a code block
+            if block_is_code(markdown_block):
+                return BlockType.CODE
+        case ">":  # check if block is a quote block
+            if block_is_quote(markdown_block):
+                return BlockType.QUOTE
+        case "-":  # possible unordered list
+            if block_is_unordered_list(markdown_block):
+                return BlockType.UNORDERED_LIST
+        case "1":  # possible ordered list
+            if block_is_ordered_list(markdown_block):
+                return BlockType.ORDERED_LIST
+
+        # case _:  # no matches; default back to "normal" paragraph
+        #    return BlockType.PARAGRAPH
+    return BlockType.PARAGRAPH
+
+
+def block_is_heading(markdown_block):
+    num_leading_hashes = 0
+    current_char = markdown_block[num_leading_hashes]
+    while current_char == "#":
+        num_leading_hashes += 1
+
+        # require that markdown block must have characters afterwards
+        assert len(markdown_block) >= num_leading_hashes
+
+        current_char = markdown_block[num_leading_hashes]
+
+    # minimum 1, maximum 6 hashes for heading blocks
+    if (1 <= num_leading_hashes) and (num_leading_hashes <= 6):
+        # hashes must be followed by a space
+        if markdown_block[num_leading_hashes] == " ":
+            # space must be followed by text
+            # TODO: better check for if title text is valid? (e.g. not just whitespace?)
+            if len(markdown_block) > num_leading_hashes + 1:
+                return True
+
+    return False
+
+
+def block_is_code(markdown_block):
+    # TODO: check for whether the block doesn't have ``` in the middle?
+    if markdown_block.startswith("```") and markdown_block.endswith("```"):
+        return True
+    return False
+
+
+def block_is_quote(markdown_block):
+    # split into lines, to check whether they start with ">"
+    split_lines = markdown_block.split("\n")
+    for line in split_lines:
+        if not line.startswith(">"):
+            return False
+
+    return True
+
+
+def block_is_unordered_list(markdown_block):
+    # split into lines, to check whether they start with "- "
+    split_lines = markdown_block.split("\n")
+    for line in split_lines:
+        if not line.startswith("- "):
+            return False
+
+    return True
+
+
+def block_is_ordered_list(markdown_block):
+    # split into lines, to check whether they start with "X. ", where X is the item number
+    split_lines = markdown_block.split("\n")
+    item_number = 1  # check whether the number starting in the line is correct
+    for line in split_lines:
+        string_to_check_start = str(item_number) + ". "
+        if not line.startswith(string_to_check_start):
+            return False
+        item_number += 1
+
+    return True
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +45,77 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading_1hash(self):
+        md_block = """# Title example
+followed by lots of text
+in the paragraph that follows"""
+        expected_result = BlockType.HEADING
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_heading_6hash(self):
+        md_block = "###### heading with 6 hashes"
+        expected_result = BlockType.HEADING
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_heading_10hash_invalid(self):
+        md_block = """########## invalid heading with 10 hashes
+text comes afterwards too """
+        expected_result = BlockType.PARAGRAPH
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_code_block(self):
+        md_block = """``` example of code
+        which is spread
+        across mulitple lines```"""
+        expected_result = BlockType.CODE
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_code_block_no_ending_invalid(self):
+        md_block = """``` example of code,
+but with no ending
+it just keeps going"""
+        expected_result = BlockType.PARAGRAPH
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_quote_block(self):
+        md_block = """> Dorothy followed her through many of the beautiful rooms in her castle.
+>
+> The Witch bade her clean the pots and kettles and sweep the floor and keep the fire fed with wood."""
+        expected_result = BlockType.QUOTE
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_unordered_list(self):
+        md_block = """- items in a list
+- not any particular order
+- across multiple lines
+- spaces after the bullet """
+        expected_result = BlockType.UNORDERED_LIST
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_ordered_list_valid(self):
+        md_block = """1. ordered list
+2. everything is ordered starting from 1
+3. no gaps, spaces are in order"""
+        expected_result = BlockType.ORDERED_LIST
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_ordered_list_invalid_numbers(self):
+        md_block = """1. This is a failed ordered list
+3. We jump over a few numbers
+2. They exist, but not in the right order
+5. Or sometimes, we forget that 4 exists"""
+        expected_result = BlockType.PARAGRAPH
+        self.assertEqual(expected_result, block_to_block_type(md_block))
+
+    def test_paragraph_no_matching_start_character(self):
+        md_block = """Just a plain ol' paragraph
+No fancy characters for me, no sir.
+It ain't much, but it's honest work."""
+        expected_result = BlockType.PARAGRAPH
+        self.assertEqual(expected_result, block_to_block_type(md_block))
 
 
 if __name__ == "__main__":
