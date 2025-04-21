5f6487568a0e5e131e36ff59521b9e24ebb65420
Jacob Christiansen <22478132+jacobfc@users.noreply.github.com>
2025-04-21 13:17:12 +0200

    Supplemented tets for block_to_block_types from bootdev

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -117,6 +117,20 @@ It ain't much, but it's honest work."""
         expected_result = BlockType.PARAGRAPH
         self.assertEqual(expected_result, block_to_block_type(md_block))
 
+    def test_block_to_block_types_from_bootdev(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
 
 if __name__ == "__main__":
     unittest.main()
