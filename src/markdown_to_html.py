b43b83cf7ec35e7d359d25a2f4361ac25ccbaff5
Jacob Christiansen <22478132+jacobfc@users.noreply.github.com>
2025-06-05 15:08:55 +0200

    finished function markdown_to_html_node(), which converts a full markdown document to html

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -2,14 +2,14 @@ from markdown_blocks import markdown_to_blocks, BlockType, block_to_block_type
 from textnode import TextNode, TextType, text_node_to_html_node
 from inline_markdown import text_to_textnodes
 
-from htmlnode import LeafNode, ParentNode
+from htmlnode import ParentNode
 
 
 def text_to_children(text):
     list_of_textnodes = text_to_textnodes(text)
     list_of_htmlnodes = []
     for textnode in list_of_textnodes:
-        list_of_textnodes.append(text_node_to_html_node(textnode))
+        list_of_htmlnodes.append(text_node_to_html_node(textnode))
     return list_of_htmlnodes
 
 
@@ -24,17 +24,19 @@ def markdown_to_html_node(markdown):
         match block_type:
             # special case for code blocks; no inline parsing of children
             case BlockType.CODE:
-                print(f"\nblock: {block}")
-                # TODO: strip "```" characters from text
-                code_block = TextNode(block, TextType.CODE_TEXT)
-                print(f"\ncode_block: {code_block}")
+                # strip "```" from both the beginning and end of the block,
+                # and strip whitespace at the beginning of the block with lstrip()
+                stripped_block = block.strip("`").lstrip()
+                code_block = TextNode(stripped_block, TextType.CODE_TEXT)
                 html_code_node = text_node_to_html_node(code_block)
                 # add <pre> wrapper (ParentNode) for multi-line code blocks
                 html_node_with_pre = ParentNode("pre", [html_code_node])
                 blocks_output_list.append(html_node_with_pre)
 
             case BlockType.PARAGRAPH:
-                children_html_nodes = text_to_children(block)
+                lines = block.split("\n")
+                paragraph = " ".join(lines)
+                children_html_nodes = text_to_children(paragraph)
                 parent_block_node = ParentNode("p", children_html_nodes)
                 blocks_output_list.append(parent_block_node)
 
@@ -49,35 +51,48 @@ def markdown_to_html_node(markdown):
                     current_char = block[num_leading_hashes]
 
                 header_string = "h" + str(num_leading_hashes)
-                # TODO: remove hashes from heading text?
-                # cleaned_block =
-                children_html_nodes = text_to_children(block)
-
+                # remove and space hashes from start of text
+                cleaned_block = block[num_leading_hashes + 1 :]
+                children_html_nodes = text_to_children(cleaned_block)
                 parent_block_node = ParentNode(header_string, children_html_nodes)
                 blocks_output_list.append(parent_block_node)
 
             case BlockType.QUOTE:
-                # TODO: remove "<" from quote lines?
-                children_html_nodes = text_to_children(block)
+                lines = block.split("\n")
+                new_lines = []
+                for line in lines:
+                    if not line.startswith(">"):
+                        raise ValueError("invalid quote block")
+                    # remove ">" and whitespace from markdown text
+                    new_lines.append(line.lstrip(">").strip())
+
+                content = " ".join(new_lines)
+                children_html_nodes = text_to_children(content)
                 parent_block_node = ParentNode("blockquote", children_html_nodes)
                 blocks_output_list.append(parent_block_node)
 
             case BlockType.UNORDERED_LIST:
-                children_html_nodes = text_to_children(block)
-                # TODO: add wraping of <li> tags, and remove "- " from list items?
-                parent_block_node = ParentNode("ul", children_html_nodes)
+                items = block.split("\n")
+                html_items = []
+                for item in items:
+                    text = item[2:]  # remove "- " from list items
+                    children = text_to_children(text)
+                    html_items.append(ParentNode("li", children))
+                parent_block_node = ParentNode("ul", html_items)
                 blocks_output_list.append(parent_block_node)
 
             case BlockType.ORDERED_LIST:
-                children_html_nodes = text_to_children(block)
-                # TODO: add wraping of <li> tags, and remove "X. " from list items?
-                parent_block_node = ParentNode("ol", children_html_nodes)
+                items = block.split("\n")
+                html_items = []
+                for item in items:
+                    text = item[3:]  # remove "X. " from list items
+                    children = text_to_children(text)
+                    html_items.append(ParentNode("li", children))
+                parent_block_node = ParentNode("ol", html_items)
                 blocks_output_list.append(parent_block_node)
 
             case _:
                 raise ValueError("No valid BlockType value")
 
-    # TODO: convert blocks_output_list to single parent HTML node (div class?)
-    print(f"\nblocks_output_list: {blocks_output_list}")
     html_parent = ParentNode("div", blocks_output_list)
     return html_parent
