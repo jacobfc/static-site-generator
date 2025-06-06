366812ee63cd895c929f69aa9cbaaf18352f1341
Jacob Christiansen <22478132+jacobfc@users.noreply.github.com>
2025-06-06 10:11:54 +0200

    Added function to copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from textnode import TextType, TextNode
+import os
+import shutil
+from copy_recursive import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 
 def main():
-    dummy_textnode = TextNode("This is sample text: Lorem Ipsum", TextType.BOLD_TEXT)
-    print(dummy_textnode)
+    abs_dest_dir = os.path.abspath(dir_path_public)
+    # delete contents of destination directory, if it exists
+    if os.path.exists(abs_dest_dir):
+        print(f"Deleting pre-existing destination directory: {abs_dest_dir}")
+        shutil.rmtree(abs_dest_dir)
+    else:
+        print("No pre-existing destination direcory")
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 if __name__ == "__main__":
