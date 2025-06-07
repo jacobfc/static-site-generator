e524739d0fe28275ea36e4b40cb18d43ba946f8c
Jacob Christiansen <22478132+jacobfc@users.noreply.github.com>
2025-06-07 19:48:12 +0200

    Added generation of additional pages - should be generalized

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from generate_page import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -19,7 +21,24 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_page("./content/index.md", template_path, "./public/index.html")
+
+    generate_page(
+        "./content/blog/glorfindel/index.md",
+        template_path,
+        "./public/blog/glorfindel/index.html",
+    )
+    generate_page(
+        "./content/blog/tom/index.md", template_path, "./public/blog/tom/index.html"
+    )
+    generate_page(
+        "./content/blog/majesty/index.md",
+        template_path,
+        "./public/blog/majesty/index.html",
+    )
+    generate_page(
+        "./content/contact/index.md", template_path, "./public/contact/index.html"
+    )
 
 
 if __name__ == "__main__":
